099b0282a95197d21ed7fe5535c7be6f53c10774
MaybeRT-rt <maybe.rt@icloud.com>
2023-08-23 19:17:29 +0300

    add networkservice

--- Navigation/Service/AppConfiguration/AppConfiguration.swift
+++ Navigation/Service/AppConfiguration/AppConfiguration.swift
@@ -8,16 +8,16 @@
 import Foundation
 
 enum AppConfiguration {
-    case people, films, planet
+    case people, films, planets
     
     var url: URL {
         switch self{
         case .people:
-            return URL(string: "https://swapi.dev/api/peoples/7")!
+            return URL(string: "https://swapi.dev/api/peoples/3")!
         case .films:
-            return URL(string: "https://swapi.dev/api/films/10")!
-        case .planet:
-            return URL(string: "https://swapi.dev/api/planets/5")!
+            return URL(string: "https://swapi.dev/api/films/3")!
+        case .planets:
+            return URL(string: "https://swapi.dev/api/planets/7")!
         }
     }
 }
--- Navigation/Service/NetworkService.swift
+++ Navigation/Service/NetworkService.swift
@@ -7,9 +7,34 @@
 
 import Foundation
 import UIKit
-
+//error code: -1009
 struct NetworkService {
-    static func request() {}
-    
-    
+    static func request(for configuration: AppConfiguration, completion: @escaping (Result<String, Error>) -> Void) {
+        let session = URLSession.shared
+        let task = session.dataTask(with: configuration.url) { data, response, error in
+            if let error = error {
+                if let urlError = error as? URLError {
+                    if urlError.code == .notConnectedToInternet {
+                        print("No internet connection: \(urlError.localizedDescription)")
+                    } else {
+                        print("Network error: \(urlError.localizedDescription)")
+                    }
+                } else {
+                    print("Error: \(error.localizedDescription)")
+                }
+            }
+            
+            if let data = data, let dataString = String(data: data, encoding: .utf8) {
+                print("Data:", dataString)
+                
+                if let httpResponse = response as? HTTPURLResponse {
+                    print("Status Code:", httpResponse.statusCode)
+                    print("Headers:", httpResponse.allHeaderFields)
+                }
+                
+                completion(.success(dataString))
+            }
+        }
+        task.resume()
+    }
 }
